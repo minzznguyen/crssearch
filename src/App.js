42cc07d60fe3fe3c52b68a37be45d9dcc432e654
minzznguyen <94585907+minzznguyen@users.noreply.github.com>
2023-05-19 14:08:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import LoginPage from './pages/login';
+import DetailsPage from './pages/details';
+import ProgressPage from './pages/progress';
+import FinderPage from './pages/finder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<LoginPage/>}></Route>
+        <Route path="/details" element = {<DetailsPage/>}></Route>
+        <Route path="/find" element = {<FinderPage/>}></Route>
+        <Route path="/progress" element = {<ProgressPage/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/finder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinderPage = () => {
+  return (
+    <div>FinderPage</div>
+  )
+}
+
+export default FinderPage
