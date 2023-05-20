cb2caa5d1ff19dde9c32f6a3f1b40be939c3f5dc
minzznguyen <94585907+minzznguyen@users.noreply.github.com>
2023-05-20 12:43:32 -0400

    basic find structure

--- src/pages/finder.js
+++ src/pages/finder.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const FinderPage = () => {
+  const dummyList = [
+    {
+      code: "EGN 3000",
+      title: "Foundation of Engineering",
+      prerequisite: ["EGN 3000", "EGN 3000L", "EGN 3001", "EGN 3002"],
+    },
+    {
+      code: "EGN 3000",
+      title: "Foundation of Engineering",
+      prerequisite: ["EGN 3000", "EGN 3000L", "EGN 3001", "EGN 3002"],
+    },
+    {
+      code: "EGN 3000",
+      title: "Foundation of Engineering",
+      prerequisite: ["EGN 3000", "EGN 3000L", "EGN 3001", "EGN 3002"],
+    },
+  ];
   return (
-    <div>FinderPage</div>
-  )
-}
+    <div>
+      <>FinderPage</>
+      <p>---------------------------------------------------</p>
+      {/* forEach doesnt return anything, while map return a list of tags */}
+      {dummyList.map((course) => (
+        <div>
+          <h3>{course.code}: {course.title}</h3>
+          <p>{course.prerequisite.join(' --> ')}</p>
+          <p>---------------------------------------------------</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default FinderPage
+export default FinderPage;
